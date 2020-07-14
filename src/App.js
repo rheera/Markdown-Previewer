b499b71364789ee905a5d78a8e9b9fc87ab7437b
Raman Heera <rheera2@uwo.ca>
2020-07-13 23:26:57 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import ds from './default-state'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-}
+//TODO get markdown parser to parse the string ds when we return it in the render also change the previewer class from p
+// to some sort of text box that's uneditable
+class ControlledInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ds
+        };
+    }
 
-export default App;
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        });
+    }
+
+    render() {
+
+        return (
+            <div className="container-fluid">
+                <div>
+                    <div id={"editor-div"}>
+                        <h4 className={"text-primary"}>Editor:</h4>
+                        <textarea id="editor" value = {this.state.input} onChange={this.handleChange.bind(this)}/>
+                    </div>
+                    <div id={"previewer-div"}>
+                        <h4 className="text-primary">Previewer:</h4>
+                        <p>{this.state.input}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+};
+
+export default ControlledInput;
---
+++ src/default-state.js
@@ -0,0 +1,16 @@
+const DEFAULTSTATE =
+    `
+    Header H1
+    Subheader H2
+    There's also [links](https://www.freecodecamp.com)
+    Here's some code, \`<div></div>\`,
+    A code block
+    list item
+    block quote
+    image
+    bolded text`;
+
+const ds = "Header H1\nSubheader H2\nThere's also [links](https://www.freecodecamp.com)\n" +
+    "Here's some code, `<div></div>`,\nA code block\nlist item\nblock quote\nimage\nbolded text"
+
+export default ds
